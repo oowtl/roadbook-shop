a91df243dfd35c95b20ae081933a9dfba988190f
oowtl <oowtl7425@gmail.com>
2024-10-20 14:49:52 +0900

    chore : spring security 설정 변경 - 로그인 페이지 설정 - 로그아웃 페이지 설정

--- src/main/java/com/shop/shop2/config/SecurityConfig.java
+++ src/main/java/com/shop/shop2/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity //  SpringSecurityFilterChain 이 자동 포함된다.
@@ -14,7 +15,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.build();
+
+        return http
+//                .authorizeHttpRequests(authorizeHttpRequestsCustomizer -> authorizeHttpRequestsCustomizer
+//                        .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+//                        .requestMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .anyRequest()
+//                        .authenticated())
+                .formLogin(formLoginCustomizer -> formLoginCustomizer
+                                .loginPage("/members/login") // 로그인 페이지 url 설정
+                                .defaultSuccessUrl("/") // 로그인 성공 시 이동 url
+                                .usernameParameter("email") // 로그인 성공 시 사용할 파라미터 타입 설정
+                                .failureUrl("/members/login/error") // 로그인 실패 시 이동 url
+//                        .failureHandler(new CustomAuthenticationFailureHandler())
+                ).logout(logoutCustomizer -> logoutCustomizer
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) // 로그아웃 url 설정
+                        .logoutSuccessUrl("/") // 로그아웃 성공 시 url 설정
+
+                )
+                .build();
 
     }
 

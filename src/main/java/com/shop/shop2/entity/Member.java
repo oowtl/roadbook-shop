9aa30243154e1365a5370df60e18d4f9b373d544
oowtl <oowtl7425@gmail.com>
2024-10-20 20:30:43 +0900

    feat : 권한 추가 - 페이지 별 권한 추가

--- src/main/java/com/shop/shop2/entity/Member.java
+++ src/main/java/com/shop/shop2/entity/Member.java
@@ -42,7 +42,8 @@ public class Member {
         String password = passwordEncoder.encode(memberFormDto.getPassword());
         member.setPassword(password);
 
-        member.setRole(Role.USER);
+//        member.setRole(Role.USER);
+        member.setRole(Role.ADMIN);
         return member;
     }
 }
